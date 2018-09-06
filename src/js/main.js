82bef40a1093b509a8d87c5744e1d0f8647bfd30
Ruslan Bagautdinov <molodoybaja@gmail.com>
2018-09-06 13:34:44 +0500

    first

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+(function() {
+  'use strict';
+
+  setTimeout(function() {
+    document.querySelector('.greating_picture').classList.add('m--show');
+  }, 1000);
+  console.log("hello world");
+})();
